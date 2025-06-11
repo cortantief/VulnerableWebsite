61eba10a3cde50a74f393fb1eb9ede721c2bf3b0
Harbouli Soufiane <sharbouli@myges.fr>
2025-06-11 11:36:43 +0200

    wip

--- index.php
+++ index.php
@@ -40,16 +40,6 @@
     </div>
   </div>
 
-  <?php
-  #$pages= array('about.php','accueil.php','services.php','works.php','membres.php');
-    if (isset($_GET['page'])){
-      include($_GET['page']);
-    }
-    else{
-      include("accueil.php");
-    }
-    
-  ?>
   <!-- /.modal -->
 
   <div id="copyrights">
